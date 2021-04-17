da7133f1a0785aaad0d2e5acb7c1e14a089af18a
Patil-94 <ankitakhairnar10@.com>
2021-04-17 16:57:18 +0530

    [Ankita] Add:Valid Email

--- Program.cs
+++ Program.cs
@@ -11,40 +11,44 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {     //Entery point 
-            
+
             Console.WriteLine("-------Welcome To User Registration--------");
             Console.WriteLine();
-            string lastName = "^[A-Z]{1}[A-Za-z]{2,}$"; //pattern using for string and string has minimum 3 char.
-            string[] lastNamearr = { "Khairnar", "Patil", "shinde", "aher", "Ah" };  //Define some  name strings.
-
+            string Pattern = "^([a-z]{3,})([.]{0,1}[a-z]*)@([a-z]{2}).([a-z]{2})([.]{1}[a-z]{2}){0,1}$"; //pattern using for email.
+         
             Program p = new Program(); // create object
-            p.ValidName(lastNamearr, lastName);
+            p.ValidName(Pattern);
             Console.WriteLine();
             Console.Read();
         }
 
-        public void ValidName(string[]arr, string lastName) //method to validate string
+        public void ValidName(string Pattern) //method to validate string
         {
-            Console.WriteLine("Validation Of The Name");
-            Regex regex = new Regex(lastName);  
-            IterateLoop(arr, regex);  //calling method
+            Console.WriteLine("Validation Of The Email");
+            Regex regex = new Regex(Pattern);
+            IterateLoop(regex);  //calling method
         }
 
-        public void IterateLoop(string[]arr, Regex regex)
+        public void IterateLoop(Regex regex)
         {
-            for (int i = 0; i < arr.Length; i++)    //check each name string using for loop 
-
+            int i = 0; //initialize i value
+            while (i != 1) 
             {
-               bool  result = regex.IsMatch(arr[i]);   //call the IsMatch metod to determine whether a match is present
-                if (result == true)  //check result is true or not using if and hence using bool
+                Console.WriteLine("Enter Email");
+                string email = Console.ReadLine();
+                bool result = regex.IsMatch(email);  //call the IsMatch metod to determine whether a match is present
+                
+                if (result == true)  ////check result is true or not using if and hence using bool
                 {
-                    Console.WriteLine(arr[i] + "---->" + "Valid");
+                    Console.WriteLine("Valid email");
+                    i = 1;
                 }
                 else
                 {
-                    Console.WriteLine(arr[i] + "--->" + "Invalid LastName");
+                    Console.WriteLine("Enter email in this format : abc.xyz@bl.co.in");
                 }
-            } //end loop
+               
+            }
         }
     }
 }
